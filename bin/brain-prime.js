eaa8223288a88670812c6d7f8e157f5967d3aa89
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-07-06 18:16:05 +0400

    modified bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import startGame from '../src/index.js';
-import { conditionGame, gameGeneration } from '../src/games/brain-prime.js';
+import launchGame from '../src/games/prime.js';
 
-startGame(conditionGame, gameGeneration);
+launchGame();
