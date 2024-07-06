a31cd6404dcfdab4a5d530a6a79c4d549c9d8f5a
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-07-06 18:38:23 +0400

    rename and modified src/games/brain-prime.js -> src/games/prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,27 @@
+import generateRandomNumber from '../utils.js';
+import startGame from '../index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const generateRound = () => {
+  const randomNumber = generateRandomNumber(1, 100);
+
+  const answer = isPrime(randomNumber) ? 'yes' : 'no';
+  const question = randomNumber;
+  return [question, answer];
+};
+
+const launchGame = () => {
+  startGame(description, generateRound);
+};
+
+export default launchGame;
