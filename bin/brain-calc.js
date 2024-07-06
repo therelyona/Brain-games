624bc9d923b042c78f134a96070384d78574610c
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-07-06 18:14:33 +0400

    modified bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import startGame from '../src/index.js';
-import { conditionGame, gameGeneration } from '../src/games/brain-calc.js';
+import launchGame from '../src/games/calc.js';
 
-startGame(conditionGame, gameGeneration);
+launchGame();
